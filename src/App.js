d4b60329ab62b3a89af8c005d969f80fbd001c34
Lina Choi <choilina16@gmail.com>
2022-06-17 23:21:50 -0400

    adding our goals + homeowners section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import React from 'react';
+import OurGoals from './components/OurGoals/OurGoals';
+import Homeowners from './components/Homeowners/Homeowners';
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      {/* for styling purposes */}
+      <OurGoals />
+      <Homeowners />
+    </div>
+  );
 }
 
 export default App;
--- src/components/OurGoals/OurGoals.js
+++ src/components/OurGoals/OurGoals.js
@@ -1,1 +1,38 @@
-// lc
+import React from 'react';
+import './ourGoals.css';
+import worker from '../../assets/ourGoalsWorker.png';
+
+function OurGoals() {
+  return (
+    <div class="container">
+      <div class="row">
+        <div class="col-12 goals-logo-title">
+          <img src={worker} alt="construction worker icon"></img>
+          <h1>OUR GOAL</h1>
+        </div>
+
+        <div class="col-6 goals-details">
+          <p>
+            All American Doors & Windows Inc. is Simi Valley’s local source for
+            quality doors, windows, moldings, and a wide variety of other
+            materials. It's both our job and great pride to assist you in
+            finding the most up-to-date materials possible!
+          </p>
+        </div>
+        <div class="col-6 goals-details">
+          <p>
+            We offer an excellent selection with the most popular brands, plus
+            superior personal service. Our goal is to give you options, help you
+            make decisions, and offer you affordable prices.
+          </p>
+        </div>
+
+        <div class="col-12 goals-button">
+          <button class="goals-button">CONTACT US</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default OurGoals;
