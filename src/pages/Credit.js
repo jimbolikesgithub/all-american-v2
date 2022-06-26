b66f70074cd12f2ddca317ba800a4ba1de24de88
Lina Choi <choilina16@gmail.com>
2022-06-26 18:11:35 -0400

    update

--- src/pages/Credit.js
+++ src/pages/Credit.js
@@ -2,60 +2,78 @@ import React from 'react';
 
 const Credit = () => {
   return (
-    <div>
+    <section>
       <h1 style={{ textAlign: 'center', padding: '30px' }}>CREDITS</h1>
+      <p>All icons used to help create this website were found on Flaticon</p>
 
-      {/* PRODUCTS PAGE */}
-      <a href="https://www.flaticon.com/free-icons/door" title="door icons">
-        Door icons created by kerismaker - Flaticon
-      </a>
-      <a href="https://www.flaticon.com/free-icons/window" title="window icons">
-        Window icons created by winnievinzence - Flaticon
-      </a>
-      <a
-        href="https://www.flaticon.com/free-icons/door-knob"
-        title="door knob icons"
+      <div
+        style={{
+          textAlign: 'center',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
       >
-        Door knob icons created by surang - Flaticon
-      </a>
+        {/* PRODUCTS PAGE */}
+        <a href="https://www.flaticon.com/free-icons/door" title="door icons">
+          Door icons created by kerismaker - Flaticon
+        </a>
+        <a
+          href="https://www.flaticon.com/free-icons/window"
+          title="window icons"
+        >
+          Window icons created by winnievinzence - Flaticon
+        </a>
+        <a
+          href="https://www.flaticon.com/free-icons/door-knob"
+          title="door knob icons"
+        >
+          Door knob icons created by surang - Flaticon
+        </a>
 
-      <a
-        href="https://www.flaticon.com/free-icons/architecture"
-        title="architecture icons"
-      >
-        Architecture icons created by Freepik - Flaticon
-      </a>
+        <a
+          href="https://www.flaticon.com/free-icons/architecture"
+          title="architecture icons"
+        >
+          Architecture icons created by Freepik - Flaticon
+        </a>
 
-      {/* OUR FEATURES SECTION */}
-      <a
-        href="https://www.flaticon.com/free-icons/construction"
-        title="construction icons"
-      >
-        Construction icons created by Freepik - Flaticon
-      </a>
+        {/* OUR FEATURES SECTION */}
+        <a
+          href="https://www.flaticon.com/free-icons/construction"
+          title="construction icons"
+        >
+          Construction icons created by Freepik - Flaticon
+        </a>
 
-      <a href="https://www.flaticon.com/free-icons/hammer" title="hammer icons">
-        Hammer icons created by Freepik - Flaticon
-      </a>
+        <a
+          href="https://www.flaticon.com/free-icons/hammer"
+          title="Customizable Icon"
+        >
+          Hammer icons created by Freepik - Flaticon
+        </a>
 
-      <a
-        href="https://www.flaticon.com/free-icons/best-price"
-        title="best price icons"
-      >
-        Best price icons created by Freepik - Flaticon
-      </a>
+        <a
+          href="https://www.flaticon.com/free-icons/best-price"
+          title="Affordable Icon"
+        >
+          Best price icons created by Freepik - Flaticon
+        </a>
 
-      <a href="https://www.flaticon.com/free-icons/family" title="family icons">
-        Family icons created by Freepik - Flaticon
-      </a>
+        <a
+          href="https://www.flaticon.com/free-icons/family"
+          title="Family-Owned Icon"
+        >
+          Family icons created by Freepik - Flaticon
+        </a>
 
-      <a
-        href="https://www.flaticon.com/free-icons/process"
-        title="process icons"
-      >
-        Process icons created by Eucalyp - Flaticon
-      </a>
-    </div>
+        <a
+          href="https://www.flaticon.com/free-icons/process"
+          title="Service Icon"
+        >
+          Process icons created by Eucalyp - Flaticon
+        </a>
+      </div>
+    </section>
   );
 };
 
